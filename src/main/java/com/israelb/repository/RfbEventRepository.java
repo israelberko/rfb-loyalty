42adaa585514ec3b0adc09885ffc95b64e38c617
israelb <israel.berko@cellwize.com>
2018-10-10 23:05:40 +0300

    adding functions to repositories

--- src/main/java/com/israelb/repository/RfbEventRepository.java
+++ src/main/java/com/israelb/repository/RfbEventRepository.java
@@ -1,9 +1,11 @@
 package com.israelb.repository;
 
 import com.israelb.domain.RfbEvent;
+import com.israelb.domain.RfbLocation;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 
 /**
  * Spring Data  repository for the RfbEvent entity.
@@ -11,5 +13,9 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface RfbEventRepository extends JpaRepository<RfbEvent, Long> {
+    RfbEvent findByRfbLocationAndEventDate(RfbLocation location, LocalDate date);
 
+    RfbEvent findByEventCodeEqualsAndEventDateEqualsAndRfbLocationEquals(String eventCode, LocalDate eventDate, RfbLocation location);
+
+    RfbEvent findByEventDateEqualsAndRfbLocationEquals(LocalDate eventDate, RfbLocation location);
 }
--- src/main/java/com/israelb/repository/RfbLocationRepository.java
+++ src/main/java/com/israelb/repository/RfbLocationRepository.java
@@ -4,6 +4,7 @@ import com.israelb.domain.RfbLocation;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 /**
  * Spring Data  repository for the RfbLocation entity.
@@ -11,5 +12,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface RfbLocationRepository extends JpaRepository<RfbLocation, Long> {
+    List<RfbLocation> findAllByRunDayOfWeek(Integer dayOfWeek);
 
+    RfbLocation findByLocationName(String name);
 }
